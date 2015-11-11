b42ceaec422f6a7b287fe14877abdaf6b9fd9276
Felipe Lenz <felipelenzc@gmail.com>
2015-11-11 15:04:22 -0500

    Cleaned up the code

--- Skywave_minus_60Hz_slope_general.py
+++ Skywave_minus_60Hz_slope_general.py
@@ -3,13 +3,10 @@
 Created on Mon Sep 28 14:56:12 2015
 
 @author: lenz
-This code plots the skywaves associated with the biggest currents for 6
-events. Skywave_plot_with_IRIG_LPF returns: moving_avg[0] (time list), 
-moving_avg[1] (filtered skywave (moving average) list), and moving_avg[2] 
-(UTC_time string)
+This code plots the skywaves from natural lightning NLDN data
+Skywave_plot_general returns: time, skywave,UTC_time,t0 
 
-[10:-10] is added when plotting to avoid showing the overshoot at the ends of
-the waveforms causeed by the moving average filter
+It also removes the 60 Hz slope
 """
 from Skywaves_plot_general import Natural_Skywaves
 from remove_60Hz_slope import remove_slope
@@ -23,21 +20,17 @@ matplotlib.rcParams.update({'font.size': 16})
 x_min=0*1e6
 x_max=0.5
 
-#distance=208.9
-#dt_70km=(2*np.sqrt(70*70+(distance/2)*(distance/2))-distance)/2.99e5 #time delay of the first skywave for ionospheric reflection height=70km
-#dt_80km=(2*np.sqrt(80*80+(distance/2)*(distance/2))-distance)/2.99e5 #time delay of the first skywave for ionospheric reflection height=80km
-#dt_90km=(2*np.sqrt(90*90+(distance/2)*(distance/2))-distance)/2.99e5 #time delay of the first skywave for ionospheric reflection height=90km
-#print("70km = %r, 80 km = %r, 90 km = %r (in microseconds)"%(dt_70km*1e6,dt_80km*1e6,dt_90km*1e6))
 
 ## Input Parameters ##
-RS_time=34.0
+RS_time=56.3 #From NLDN
 date=11072015
 fs=10e6
-suffix=1206
+suffix=1244
 DBY_station=(28.462991, -80.707441)
-NLDN_flash=(27.566,-81.851)#(29.265,-79.649)
-Peak_Current=-42
+NLDN_flash=(27.679,-81.966) #From NLDN
+Peak_Current=-16 #From NLDN
 
+# Code starts here
 Horizontal_Distance=great_circle(DBY_station, NLDN_flash).meters
 Horizontal_Distance_km=Horizontal_Distance/1000
 
@@ -48,8 +41,7 @@ print("angle= %f degrees" %angle_deg )
 print("Distance from the flash= %f km" %Horizontal_Distance_km)
 
 time,skywave,UTC_time,t0=Natural_Skywaves(RS_time,date,fs,suffix,Horizontal_Distance,x_max)
-plt.plot(time,skywave)
-plt.show()
+
 m,b,x0,xf=remove_slope(time,skywave,fs,t0)
 
 yoffset=np.mean(skywave[int(x0*fs):int(x0*fs)+800])
@@ -69,20 +61,11 @@ yoffset=yoffset+slope
 plt.figure(figsize=(11,8.5))
 plt.plot(time,skywave-yoffset,linewidth=2.0)
 #plt.plot(time*1e6,yoffset,linewidth=2.0)
-#plt.plot([moving_avg[5]*1e6,moving_avg[5]*1e6],[-1,1],'--',linewidth=2.0) #time when skywave raises 3 std dev from mean noise
-#plt.plot([(moving_avg[5]+dt_70km)*1e6,(moving_avg[5]+dt_70km)*1e6],[-1,1],'--',linewidth=2.0) #time for 70 km h iono
-#plt.plot([(moving_avg[5]+dt_80km)*1e6,(moving_avg[5]+dt_80km)*1e6],[-1,1],'--',linewidth=2.0) #time for 80 km h iono
-#plt.plot([(moving_avg[5]+dt_90km)*1e6,(moving_avg[5]+dt_90km)*1e6],[-1,1],'--',linewidth=2.0) #time for 90 km h iono
 
 plt.title("Date: "+str(date)+" \n Horizontal Distance= "+str(int(Horizontal_Distance_km))+" km, angle= "+str(angle_deg)+" deg \n Peak Current= "+str(Peak_Current)+" kA")
 plt.xlabel("UTC Time in seconds after %s" %UTC_time)
 plt.grid()
 plt.xlim(x0,xf)
-
-plt.savefig('Date= '+str(date)+' D= '+str(int(Horizontal_Distance_km))+' km, angle= '+str(angle_deg)+' deg, Peak Current= '+str(Peak_Current)+' kA.pdf', dpi = 300)
-#    #plt.ylim(-0.15,0.81)
-#
-#
+plt.tight_layout()
+plt.savefig('Date= '+str(date)+' (Lat '+str(NLDN_flash[0])+',Long '+str(NLDN_flash[1])+')D= '+str(int(Horizontal_Distance_km))+' km, angle= '+str(angle_deg)+' deg, Peak Current= '+str(Peak_Current)+' kA.pdf', dpi = 300)
 plt.show()
-#    fig_title=["UF 15-"+str(event)+", RS #"+str(RS_number[i])+".png"]
-#    savefig('fig_title')
