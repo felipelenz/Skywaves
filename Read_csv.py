b9cadf3ef3b351854205c33189317aaef8dc21b2
Felipe Lenz <felipelenzc@gmail.com>
2016-02-08 17:53:07 -0500

    Better code, plots processed data in red and puts legends in all subplots

--- Read_csv.py
+++ Read_csv.py
@@ -13,7 +13,8 @@ import scipy.fftpack
 plt.figure(figsize=(8.5,11))
 fs=10e6
 
-filename='UF 15-43, RS4 (Ez, DBY) (raw).csv'
+filename1='UF 15-43, RS4 (Ez, DBY) (raw).csv'
+filename2='UF 15-43, RS4 (Ez, DBY).csv'
 title_string='UF15-43, RS#4'
 def fft_plot(filename):
     
@@ -26,20 +27,22 @@ def fft_plot(filename):
         time.append(column[0])
         data.append(column[1])
     
-    plt.subplot(311)
-    plt.plot(time,data)
-    plt.xlabel('Time($\mu s$)')
-    plt.ylabel('Uncalibrated $E_z$ from DBY')
-    plt.title(title_string)
-    
+   
     data_array=np.asarray(data)
     
     data_FFT=scipy.fftpack.fft(data_array)
     N=len(data_array)
     xf=np.linspace(0.0,(N-1)*fs/N,N)
-    return xf, data_FFT
+    return time,data,xf,data_FFT
+    
+ 
+time,data,xf,data_FFT=fft_plot(filename1)
+plt.subplot(311)
+plt.plot(time,data,label='raw data')
+plt.xlabel('Time($\mu s$)')
+plt.ylabel('Uncalibrated $E_z$ from DBY')
+plt.title(title_string)
 
-xf,data_FFT=fft_plot(filename)
 plt.subplot(312)
 plt.plot(xf*1e-6,20*np.log10(abs(data_FFT)),label='raw data')
 plt.xlabel('Frequency (MHz)')
@@ -55,10 +58,16 @@ plt.xlim(0,fs*1e-6/2)
 plt.plot()
 #plt.show()
 
-filename='UF 15-43, RS4 (Ez, DBY).csv'
-xf,data_FFT=fft_plot(filename)
+time,data,xf,data_FFT=fft_plot(filename2)
+plt.subplot(311)
+plt.plot(time,data,'r',label='processed data')
+plt.xlabel('Time($\mu s$)')
+plt.ylabel('Uncalibrated $E_z$ from DBY')
+plt.title(title_string)
+plt.legend()
+
 plt.subplot(312)
-plt.plot(xf*1e-6,20*np.log10(abs(data_FFT)),color=[1,0,0],label='processed data')
+plt.plot(xf*1e-6,20*np.log10(abs(data_FFT)),'r',label='processed data')
 plt.xlabel('Frequency (MHz)')
 plt.ylabel('FFT Magnitude (dB)')
 plt.xlim(0,fs*1e-6/2)
@@ -66,7 +75,7 @@ plt.legend()
 
 plt.subplot(313)
 Phase=np.unwrap(np.angle(data_FFT))
-plt.plot(xf*1e-6,Phase*180/np.pi,color=[1,0,0],label='processed data')
+plt.plot(xf*1e-6,Phase*180/np.pi,'r',label='processed data')
 plt.xlabel('Frequency (MHz)')
 plt.ylabel('FFT Phase (degrees)')
 plt.xlim(0,fs*1e-6/2)
