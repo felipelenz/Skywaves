fcddb8d0ab6656ef7d2c9d132e032a504700561d
Felipe Lenz <felipelenzc@gmail.com>
2016-03-17 12:03:15 -0400

    includes half width measurement and 5-peak time delay

--- read_DBY.py
+++ read_DBY.py
@@ -134,7 +134,7 @@ def read_DBY(event,RS_number, RS_time,suffix,x_max,filterwindow):
     #        plot(x,y)
     #        print("Please click")
     #        xx = ginput(3)
-        xx=[0,0.00008,0.000095] #just for this application! 
+        xx=[0,0.00006,0.000095] #just for this application! 
         sampling_time=1/fs
     
         t0_noise=np.int(xx[0]/sampling_time)-t0/sampling_time #xx[0][0] when ginput is being used
@@ -144,6 +144,7 @@ def read_DBY(event,RS_number, RS_time,suffix,x_max,filterwindow):
         
         t_max=np.argmax(y[0:t_end])
         y_topeak=y[0:t_max]
+        y_afterpeak=y[t_max:-1]
         noise_data_window=y_topeak[t0_noise:tf_noise]
         sigma=np.std(noise_data_window)
         mean=np.mean(noise_data_window)
@@ -154,22 +155,32 @@ def read_DBY(event,RS_number, RS_time,suffix,x_max,filterwindow):
         curve_peak=y_topeak[-1]-mean
         y_ampl=max_ampl-min_ampl
        
+        five_percent_ind=np.argmax(np.abs(1.0/((0.05*y_ampl+min_ampl)-y_topeak))) 
         ten_percent_ind=np.argmax(np.abs(1.0/((0.1*y_ampl+min_ampl)-y_topeak)))  
         twenty_percent_ind=np.argmax(np.abs(1.0/((0.2*y_ampl+min_ampl)-y_topeak))) 
         fifty_percent_ind=np.argmax(np.abs(1.0/((0.5*y_ampl+min_ampl)-y_topeak))) 
+        fifty_percent_afterpeak_ind=np.argmax(np.abs(1.0/((0.5*y_ampl+min_ampl)-y_afterpeak))) + t_max
         eighty_percent_ind=np.argmax(np.abs(1.0/((0.8*y_ampl+min_ampl)-y_topeak))) 
         ninety_percent_ind=np.argmax(np.abs(1.0/((0.9*y_ampl+min_ampl)-y_topeak))) 
         risetime_90_10=ninety_percent_ind-ten_percent_ind
         risetime_90_10_time=risetime_90_10*sampling_time  
         
-    #        print("standard deviation= %.4f noise mean= %.4f"%(sigma,mean))
-    #        plot(x,y, 'b', \
-    #        x[t0_noise:tf_noise],y[t0_noise:tf_noise], 'g', \
-    #        [x[0],x[-1]],[mean,mean], 'r', \
-    #        [x[0],x[-1]],[mean+sigma,mean+sigma], '--r', \
-    #        [x[0],x[-1]],[mean-sigma,mean-sigma], '--r', \
-    #        [x[min_ind],x[ten_percent_ind],x[ninety_percent_ind]],[y[min_ind],y[ten_percent_ind],y[ninety_percent_ind]], 'or')
-    #        show()
+        five_percent_to_peak_time=(t_max-five_percent_ind)*sampling_time
+        half_width_time=(fifty_percent_afterpeak_ind-fifty_percent_ind)*sampling_time
+        print("standard deviation= %.4f noise mean= %.4f"%(sigma,mean))
+        print("10-90 risetime (sec)=%r"%risetime_90_10_time)
+        print("5-peak risetime (sec)=%r"%five_percent_to_peak_time)
+        print("half peak width (sec)=%r"%half_width_time)
+        plt.plot(x,y, 'b', \
+        x[t0_noise:tf_noise],y[t0_noise:tf_noise], 'g', \
+        [x[0],x[-1]],[mean,mean], 'r', \
+        [x[0],x[-1]],[mean+sigma,mean+sigma], '--r', \
+        [x[0],x[-1]],[mean-sigma,mean-sigma], '--r', \
+        x[min_ind],y[min_ind],'og',\
+        x[five_percent_ind],y[five_percent_ind],'oy',\
+        [x[ten_percent_ind],x[ninety_percent_ind]],[y[ten_percent_ind],y[ninety_percent_ind]], 'or',\
+        [x[fifty_percent_ind],x[fifty_percent_afterpeak_ind]],[y[fifty_percent_ind],y[fifty_percent_afterpeak_ind]],'oc')
+        plt.show()
         
         return risetime_90_10_time, ten_percent_ind, min_ind, y_ampl, mean,\
         twenty_percent_ind, fifty_percent_ind, eighty_percent_ind,\
@@ -179,7 +190,7 @@ def read_DBY(event,RS_number, RS_time,suffix,x_max,filterwindow):
     #    LPF_skywave = noise_analysis(time2,filtered_skywave,10e6,0)
     #    print("LPF 10-90 risetime = %r" %LPF_skywave[0])
     #    
-    MovAvg_skywave = noise_analysis(time,Moving_Average_Ez,fs,0) 
+    MovAvg_skywave = noise_analysis(time[10:-10],Moving_Average_Ez[10:-10],fs,0) 
     risetime_10_90=MovAvg_skywave[0]
     ten_percent_level=MovAvg_skywave[1]*(1/fs)
     ground_wave_start=MovAvg_skywave[2]*(1/fs)
